37bac2d568a6b26d36ab6ec64b803a2b48de38b7
Quan-Z-A <72383118+Quan-Z-A@users.noreply.github.com>
2020-10-24 15:40:25 +0800

    Add files via upload

---
+++ personnel.java
@@ -0,0 +1,14 @@
+package 实验二;
+
+public class personnel {    //父类
+    int number;
+    String name;
+    char sex;
+    courses a;
+    public personnel(int number, String name, char sex) {
+        this.number = number;
+        this.name = name;
+        this.sex = sex;
+
+    }
+}
